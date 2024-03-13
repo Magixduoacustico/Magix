76fb1979354ea991565785e93c5ae9bd2bfba728
Magixduoacustico <162910997+Magixduoacustico@users.noreply.github.com>
2024-03-13 10:02:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "seleccionado2";
+    opciones[1].className = "seleccionado3";
+    opciones[2].className = "seleccionado4";
+    opciones[3].className = "seleccionado5";
+  
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//detecto el scrolling para aplicar la animación del la barra de habilidades
+window.onscroll = function() { efectoHabilidades() };
+
+//funcion que aplica la animación de la barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("react").classList.add("barra-progreso4");
+        
+    }
+
+}
+document.getElementById('commentForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var commentInput = document.getElementById('comment');
+    var commentText = commentInput.value.trim();
+    
+    if (commentText !== '') {
+        var comments = getComments();
+        
+        comments.push(commentText);
+        setComments(comments);
+        
+        renderComments();
+        
+        // Limpiar el campo de comentario después de enviar
+        commentInput.value = '';
+    } else {
+        alert('Por favor, escribe un comentario antes de enviar.');
+    }
+});
+
